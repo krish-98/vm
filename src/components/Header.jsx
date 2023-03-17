7d379b9988f6efc43fe5fd599ace8dc6098e05d0
krish-98 <muralikrishnanm98@gmail.com>
2023-03-17 19:20:41 +0530

    warning notice added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,42 +4,43 @@ import { ImCross } from "react-icons/im"
 
 const Header = () => {
   const [toggle, setToggle] = useState(false)
+
   return (
     <header>
       {/* Mobile to Tab screens sizes */}
-      {/* <nav className="bg-[#F3EED9]">
-        <div className="max-w-[1180px] mx-auto">
-          <div className="flex justify-between items-center font-semibold text-[#824936] px-3 py-6">
-            <h1 className="text-2xl">VM Consultant</h1>
+      <nav className="bg-[#F3EED9] relative lg:hidden">
+        <div className="flex justify-between items-center font-semibold text-[#824936] px-3 py-6">
+          <h1 className="text-2xl">VM Consultant</h1>
 
-            {!toggle ? (
-              <GiHamburgerMenu
-                className="fill-[#824936] w-10 h-6 cursor-pointer"
-                onClick={() => setToggle(!toggle)}
-              />
-            ) : (
-              <ImCross
-                className="fill-[#824936] w-10 h-6 cursor-pointer"
-                onClick={() => setToggle(!toggle)}
-              />
-            )}
-            <ul className="hidden lg:flex justify-between gap-8 border">
+          {!toggle ? (
+            <GiHamburgerMenu
+              className="fill-[#824936] w-10 h-6 cursor-pointer"
+              onClick={() => setToggle(!toggle)}
+            />
+          ) : (
+            <ImCross
+              className="fill-[#824936] w-10 h-6 cursor-pointer"
+              onClick={() => setToggle(!toggle)}
+            />
+          )}
+          {toggle && (
+            <ul className="bg-[#F3EED9] text-[#824936] flex flex-col items-center gap-6 absolute top-20 left-0 right-0 py-4">
               <li>HOME</li>
               <li>ABOUT</li>
               <li>SERVICES</li>
               <li>CONTACT US</li>
             </ul>
-          </div>
+          )}
         </div>
-      </nav> */}
+      </nav>
 
       {/* Laptop and above screen size */}
-      <nav className="bg-[#F3EED9]">
-        <div className="max-w-[1180px] mx-auto">
+      <nav className="hidden lg:block bg-[#F3EED9]">
+        <div className="max-w-[1180px] mx-auto px-6">
           <div className="flex justify-between items-center font-semibold text-[#824936] px-3 py-6">
             <h1 className="text-2xl">VM Consultant</h1>
 
-            <ul className="hidden lg:flex justify-between gap-8 ">
+            <ul className="flex justify-between gap-8 ">
               <li>HOME</li>
               <li>ABOUT</li>
               <li>SERVICES</li>
---
+++ src/components/Main.jsx
@@ -0,0 +1,37 @@
+const Main = () => {
+  return (
+    <div className="bg-[#F3EED9] h-screen py-20 px-6">
+      <h1 className="text-3xl font-bold text-center text-red-500 lg:text-2xl">
+        Polishing our site give us some time! <br /> We will be back soon.
+      </h1>
+
+      <div className="mt-6 border border-dashed border-[#824936] p-4 md:max-w-[400px] md:mx-auto">
+        <h2 className="underline mb-2"> For immediate contact:</h2>
+        <div className="">
+          <p className=" font-bold text-[#824936] text-lg">
+            V. Mukesh (Property Realtor)
+          </p>
+
+          <p className=" font-bold text-[#824936]">
+            call: <a href="tel:+918190023139">+91 81900-23139</a>
+          </p>
+
+          <div className="font-bold text-[#824936]">
+            <p>
+              <span>mail: </span>
+              <a href="mail:vmconsultantmdu@gmail.com">
+                vmconsultantmdu@gmail.com
+              </a>
+            </p>
+
+            <p>
+              <span className="text-transparent">mail: </span>
+              <a href="mail:vmukesh775@gmail.com">vmukesh775@gmail.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Main
